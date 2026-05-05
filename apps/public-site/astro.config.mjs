cf850af3a2990e66fe0eaf6d66a6390ae1d8088e
poponotann <rakurusu77@gmail.com>
2026-05-05 17:13:48 +0900

    add cloudflare adapter

--- apps/public-site/astro.config.mjs
+++ apps/public-site/astro.config.mjs
@@ -1,8 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import cloudflare from "@astrojs/cloudflare";
+
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: cloudflare({
+    imageService: "passthrough",
+    prerenderEnvironment: "node",
+  }),
 });
